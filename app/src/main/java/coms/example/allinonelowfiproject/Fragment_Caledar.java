4cbacd0de3869ad6352d365738b46f70ceb9a261
Jayfunf <simh3077@gmail.com>
2021-05-20 20:55:38 +0900

    Test : onDateSelected for Calendar_HKB

--- app/src/main/java/coms/example/allinonelowfiproject/Fragment_Caledar.java
+++ app/src/main/java/coms/example/allinonelowfiproject/Fragment_Caledar.java
@@ -25,15 +25,7 @@ import coms.example.allinonelowfiproject.mainCalendar.OnDateSelectedListener;
 public class Fragment_Caledar extends Fragment {
 
     private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
-
     MaterialCalendarView materialCalendarView;
-    
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     public Fragment_Caledar(){
 
@@ -42,6 +34,29 @@ public class Fragment_Caledar extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //View view = inflater.inflate(R.layout.fragment_calendar,container,false);
+        //Toast.makeText(getActivity(),"Hello",Toast.LENGTH_SHORT).show();
+
+        /*materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                int Year = date.getYear();
+                int Month = date.getMonth();
+                int Day = date.getDay();
+
+                Log.i("Year test", Year + "");
+                Log.i("Month test", Month + "");
+                Log.i("Day test", Day + "");
+
+                String shot_Day = Year + "," + Month + "," + Day;
+
+                Log.i("shot_Day test", shot_Day + "");
+                materialCalendarView.clearSelection();
+
+                Toast.makeText(getActivity(), shot_Day , Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
         return inflater.inflate(R.layout.fragment_calendar, container,false);
     }
 }
